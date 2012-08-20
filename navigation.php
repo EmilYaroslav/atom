78e27a87bcf7255092838f588490d7dd606a5741
Emil Andersson <emil@sparklingscience.com>
2012-08-20 16:51:26 +0200

    Here's what I've done

---
+++ navigation.php
@@ -0,0 +1,41 @@
+<div class="navbar navbar-fixed-top">
+	<div class="navbar-inner">
+    	<div class="container-fluid">
+        
+        	<a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+        	<a class="brand" href="#"><img src="img/logo.png" /></a>
+            
+            <div class="btn-group pull-right">
+            	<a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                	<i class="icon-user"></i> Login or signup
+                    <span class="caret"></span>
+                </a>
+                
+                <ul class="dropdown-menu">
+                	<li><a href="#">Using Facebook</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">Using Open ID</a></li>
+                    <li class="divider"></li>
+                    <li><a href="#">Sign up</a></li>
+                </ul>
+            </div>
+            
+            <div class="nav-collapse">
+            	<ul class="nav">
+                	<li class="active"><a href="#">Home</a></li>
+                	<li><a href="#">Showcase</a></li>
+                	<li><a href="#">Get in touch!</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">SparkliTech Inc</a></li>
+                </ul>
+            </div>
+            
+        </div>
+    </div>
+</div>
